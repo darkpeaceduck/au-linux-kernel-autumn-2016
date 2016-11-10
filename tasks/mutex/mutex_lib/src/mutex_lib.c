e4de9407e1243f641d2f232fcd82e138b7ce381a
sanya1 <okay123456@yandex.ru>
2016-11-10 14:14:36 +0300

    task : mutex

--- tasks/mutex/mutex_lib/src/mutex_lib.c
+++ tasks/mutex/mutex_lib/src/mutex_lib.c
@@ -1,17 +1,43 @@
 #include <mutex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+
+static int mutex_sys_fd = -1;
+
+static void propagate_completed(mutex_t * m, int val) {
+	m->completed = val;
+	__sync_synchronize();
+}
 
 mutex_err_t mutex_init(mutex_t *m)
 {
     // TODO initialize userspace side mutex state
     // and create kernel space mutex state
-    return MUTEX_INTERNAL_ERR;
+	m->completed = 1;
+	m->queue_empty = 0;
+	shared_spinlock_init(&m->lock);
+	int rc = ioctl(mutex_sys_fd, MUTEX_IOCTL_CREATE,
+			&(mutex_ioctl_create_arg_t) {
+		.queue_empty = &m->queue_empty,
+		.completed = &m->completed,
+		.mutex_id = &m->id
+	});
+    return rc ? MUTEX_INTERNAL_ERR : MUTEX_OK;
 }
 
 mutex_err_t mutex_deinit(mutex_t *m)
 {
-    // TODO destroy kernel side mutex state
-    // and deinitialize userspace side.
-    return MUTEX_INTERNAL_ERR;
+	propagate_completed(m, 1);
+	int rc = ioctl(mutex_sys_fd, MUTEX_IOCTL_DESTROY,
+			&(mutex_ioctl_destroy_arg_t) {
+		.id = m->id,
+	});
+	return rc ? MUTEX_INTERNAL_ERR : MUTEX_OK;
 }
 
 mutex_err_t mutex_lock(mutex_t *m)
@@ -19,7 +45,27 @@ mutex_err_t mutex_lock(mutex_t *m)
     // TODO lock spinlock here.
     // If not successful then go to sleep
     // in kernel.
-    return MUTEX_INTERNAL_ERR;
+
+	while (!shared_spin_trylock(&m->lock)) {
+		int ret = ioctl(mutex_sys_fd, MUTEX_IOCTL_LOCK,
+				&(mutex_ioctl_lock_arg_t) {
+			.id = m->id,
+		});
+		if (ret)
+			return MUTEX_INTERNAL_ERR;
+		/* it case of avoiding race described below
+		 * here may be multiple threads - so
+		 * we cant just break here
+		 * and don't unlock spinlock if queue isn't empty in
+		 * mutex_unlock
+		 */
+	}
+	propagate_completed(m, 0);
+    return MUTEX_OK;
+}
+
+static int check_wait_queue_empty(mutex_t * m) {
+	return m->queue_empty == 0;
 }
 
 mutex_err_t mutex_unlock(mutex_t *m)
@@ -27,19 +73,43 @@ mutex_err_t mutex_unlock(mutex_t *m)
     // TODO unlock spinlock
     // and wakeup one kernel side waiter
     // if it exists.
-    return MUTEX_INTERNAL_ERR;
+
+	/*
+	 * we propagating completed info to kernel
+	 * to avoid this race:
+	 * thread1 = t1, thread2 = t2
+	 *  1) t1 entered to critical section
+	 *	2) t2 tryed to entter - failed, jumped to kernel
+	 *	3) t1 checked queue size = 0
+	 *	4) t2 went to sleep
+	 *	5) t1 exited section
+	 */
+	propagate_completed(m, 1);
+	if (!check_wait_queue_empty(m)) {
+		shared_spin_unlock(&m->lock);
+		int rc = ioctl(mutex_sys_fd, MUTEX_IOCTL_UNLOCK,
+				&(mutex_ioctl_unlock_arg_t) {
+			.id = m->id,
+		});
+		return rc ? MUTEX_INTERNAL_ERR : MUTEX_OK;
+	} else
+		shared_spin_unlock(&m->lock);
+    return MUTEX_OK;
 }
 
 mutex_err_t mutex_lib_init()
 {
-    // TODO create current process mutex
-    // registry on kernel side
-    return MUTEX_INTERNAL_ERR;
+	if (mutex_sys_fd != -1)
+		return MUTEX_INTERNAL_ERR;
+	mutex_sys_fd = open("/dev/mutex", O_RDWR);
+	return mutex_sys_fd < 0 ? MUTEX_INTERNAL_ERR : MUTEX_OK;
 }
 
 mutex_err_t mutex_lib_deinit()
 {
-    // TODO destroy current process mutex
-    // registry on kernel side
-    return MUTEX_INTERNAL_ERR;
+	if (mutex_sys_fd == -1)
+		return MUTEX_INTERNAL_ERR;
+    close(mutex_sys_fd);
+    mutex_sys_fd = -1;
+    return MUTEX_OK;
 }
--- tasks/mutex/mutex_module/module.c
+++ tasks/mutex/mutex_module/module.c
@@ -13,10 +13,33 @@
 #include <linux/rculist.h>
 #include <linux/types.h>
 #include <linux/spinlock.h>
+#include <linux/spinlock_types.h>
 #include <linux/err.h>
+#include <linux/wait.h>
 #include "mutex_ioctl.h"
 
 #define LOG_TAG "[MUTEX_MODULE] "
+
+typedef struct kernel_mutex_keeper {
+	struct rcu_head rcu;
+	struct hlist_node list;
+	wait_queue_head_t q;
+	int __user * queue_empty;
+	int __user * completed;
+	spinlock_t user;
+	mutex_id_t id;
+	int destroying;
+}kernel_mutex_keeper_t;
+
+typedef struct tgstate {
+	struct rcu_head rcu;
+	struct hlist_node list;
+	struct hlist_head mutexes;
+	spinlock_t wlock;
+	pid_t tgid;
+
+	mutex_id_t last_id;
+}tgstate_t;
 
 typedef struct system_mutex_state {
     // lock only when adding new tgroup
@@ -30,6 +53,93 @@ typedef struct mutex_dev {
 } mutex_dev_t;
 
 static mutex_dev_t *mutex_dev;
+
+typedef enum {
+	RCU,
+	DEFAULT
+} search_mode;
+
+static kernel_mutex_keeper_t * mutex_create(mutex_id_t id,
+		int __user * queue_empty,
+		int __user * completed) {
+	kernel_mutex_keeper_t * keeper = kmalloc(sizeof(kernel_mutex_keeper_t), GFP_KERNEL);
+
+	/* no rcu head initialoation */
+	INIT_HLIST_NODE(&keeper->list);
+	init_waitqueue_head(&keeper->q);
+	keeper->queue_empty = queue_empty;
+	keeper->completed = completed;
+	spin_lock_init(&keeper->user);
+	keeper->id = id;
+	keeper->destroying = 0;
+	return keeper;
+}
+
+static void mutex_shedule_release(kernel_mutex_keeper_t *keeper) {
+	hlist_del_rcu(&keeper->list);
+	kfree_rcu(keeper, rcu);
+}
+
+static tgstate_t * tg_create(void) {
+	tgstate_t * fresh = kmalloc(sizeof(tgstate_t), GFP_KERNEL);
+	INIT_HLIST_HEAD(&fresh->mutexes);
+	INIT_HLIST_NODE(&fresh->list);
+	spin_lock_init(&fresh->wlock);
+	fresh->tgid = current->tgid;
+	fresh->last_id = 0;
+	return fresh;
+}
+
+static void tg_shedule_destroy(tgstate_t * state) {
+	kfree_rcu(state, rcu);
+}
+
+static int tg_search_check(tgstate_t * state) {
+	return current->tgid == state->tgid;
+}
+
+static tgstate_t * tg_search(search_mode mode) {
+	tgstate_t * state;
+
+	if (mode == RCU) {
+		hlist_for_each_entry_rcu(state, &mutex_dev->sysmstate.tgstates, list) {
+			if(tg_search_check(state))
+				return state;
+		}
+	} else {
+		hlist_for_each_entry(state, &mutex_dev->sysmstate.tgstates, list) {
+			if(tg_search_check(state))
+				return state;
+		}
+	}
+
+	return NULL;
+}
+
+static int mutex_check(mutex_id_t id, kernel_mutex_keeper_t * keeper) {
+	return keeper->id == id;
+}
+
+static kernel_mutex_keeper_t * mutex_search(tgstate_t * state,
+		mutex_id_t id, search_mode mode) {
+	kernel_mutex_keeper_t * keeper;
+
+
+	if (mode == RCU) {
+		hlist_for_each_entry_rcu(keeper, &state->mutexes, list) {
+			if (mutex_check(id, keeper)) {
+				return keeper;
+			}
+		}
+	} else {
+		hlist_for_each_entry(keeper, &state->mutexes, list) {
+			if (mutex_check(id, keeper)) {
+				return keeper;
+			}
+		}
+	}
+	return NULL;
+}
 
 static void init_system_mutex_state(system_mutex_state_t *sysmstate)
 {
@@ -45,22 +155,223 @@ static void deinit_system_mutex_state(system_mutex_state_t *sysmstate)
 
 static int mutex_dev_open(struct inode *inode, struct file *filp)
 {
-    // TODO
+	system_mutex_state_t * state = &mutex_dev->sysmstate;
+	tgstate_t * fresh = tg_create();
+
+	spin_lock(&state->wlock);
+	fresh = tg_search(DEFAULT);
+	if (fresh != NULL) {
+		spin_unlock(&state->wlock);
+		return -EINVAL;
+	}
+	fresh = tg_create();
+	hlist_add_tail_rcu(&fresh->list, &state->tgstates);
+	spin_unlock(&state->wlock);
+
     pr_notice(LOG_TAG " opened successfully\n");
     return 0;
 }
 
 static int mutex_dev_release(struct inode *inode, struct file *filp)
 {
-    // TODO
+	tgstate_t * tg_state;
+	kernel_mutex_keeper_t * keeper;
+	struct hlist_node * tmp;
+	system_mutex_state_t * sys_state = &mutex_dev->sysmstate;
+
+	spin_lock(&sys_state->wlock);
+	tg_state = tg_search(DEFAULT);
+	if (sys_state == NULL) {
+		spin_unlock(&sys_state->wlock);
+		return -EINVAL;
+	}
+	hlist_del_rcu(&tg_state->list);
+	spin_unlock(&sys_state->wlock);
+
+	spin_lock(&tg_state->wlock);
+	hlist_for_each_entry_safe(keeper, tmp, &tg_state->mutexes, list) {
+		mutex_shedule_release(keeper);
+	}
+	spin_unlock(&tg_state->wlock);
+
+	tg_shedule_destroy(tg_state);
+
+
     pr_notice(LOG_TAG " closed\n");
     return 0;
 }
 
+static long mutex_ioctl_destroy(mutex_ioctl_destroy_arg_t __user *uarg)  {
+	mutex_ioctl_destroy_arg_t arg;
+	tgstate_t * tg_state;
+	kernel_mutex_keeper_t * keeper;
+	long rc = 0;
+
+
+	if (copy_from_user(&arg, uarg, sizeof(arg)))
+		return -EFAULT;
+
+
+	rcu_read_lock(); /* for tgstates list */
+	tg_state = tg_search(RCU);
+	if (tg_state == NULL) {
+		rc = -EFAULT;
+		goto out;
+	}
+
+	spin_lock(&tg_state->wlock);
+	keeper = mutex_search(tg_state, arg.id, DEFAULT);
+	if (keeper == NULL) {
+		rc = -EFAULT;
+		goto out_spin;
+	}
+
+	keeper->destroying = 1;
+	wake_up_all(&keeper->q);
+	mutex_shedule_release(keeper);
+out_spin:
+	spin_unlock(&tg_state->wlock);
+out:
+ 	rcu_read_unlock();
+ 	return rc;
+}
+
+static long mutex_ioctl_create(mutex_ioctl_create_arg_t __user *uarg)  {
+	mutex_ioctl_create_arg_t arg;
+	tgstate_t * tg_state;
+	kernel_mutex_keeper_t * keeper;
+	long rc = 0;
+	int new_id = 0;
+
+	if (copy_from_user(&arg, uarg, sizeof(arg)))
+		return -EFAULT;
+
+	rcu_read_lock(); /* for tgstates list */
+	tg_state = tg_search(RCU);
+	if (tg_state == NULL) {
+		rc = -EFAULT;
+		goto out;
+	}
+
+	spin_lock(&tg_state->wlock);
+	new_id = tg_state->last_id++;
+	keeper = mutex_create(new_id, arg.queue_empty, arg.completed);
+	hlist_add_tail_rcu(&keeper->list, &tg_state->mutexes);
+	spin_unlock(&tg_state->wlock);
+
+out:
+ 	rcu_read_unlock();
+ 	if (!rc && copy_to_user(arg.mutex_id, &new_id, sizeof(new_id))) {
+ 		rc = -EFAULT;
+ 	}
+ 	return rc;
+}
+
+static void mutex_update_user(kernel_mutex_keeper_t * keeper, int dec) {
+	int val;
+
+	spin_lock(&keeper->user);
+	val = *keeper->queue_empty + dec;
+	mb();
+	*keeper->queue_empty = val;
+	mb();
+	spin_unlock(&keeper->user);
+}
+
+static void mutex_wait_compeleted(kernel_mutex_keeper_t * keeper) {
+	mutex_update_user(keeper, 1);
+	mb();
+	/* condition in wait_event_exclusive_cmd
+	 * checks before sleep - don't go to sleep if crit section
+	 * owner exitedv - this is part of avoiding of race described in
+	 * mutex_lib.c
+	 */
+	wait_event_exclusive_cmd(keeper->q, (*keeper->completed) , ; , ;);
+	mutex_update_user(keeper, -1);
+}
+
+static void mutex_wake_single(kernel_mutex_keeper_t * keeper) {
+	/* wake_up - __wake_up with nr = 1 - wakes single exclusive task */
+	wake_up(&keeper->q);
+}
+
+static long mutex_ioctl_lock(mutex_ioctl_lock_arg_t __user *uarg) {
+	mutex_ioctl_lock_arg_t arg;
+	tgstate_t * state;
+	kernel_mutex_keeper_t * keeper;
+	int rc = 0;
+
+
+	if (copy_from_user(&arg, uarg, sizeof(arg)))
+		return -EFAULT;
+
+	rcu_read_lock();
+	state = tg_search(RCU);
+	if (state == NULL) {
+		rc = -EFAULT;
+		goto lock_out;
+	}
+	keeper = mutex_search(state, arg.id, RCU);
+	if (keeper == NULL) {
+		rc = -EFAULT;
+		goto lock_out;
+	}
+	mutex_wait_compeleted(keeper);
+	/* flag sets before wake_up_all, flag checks after wake -
+	 * not need additional locking here
+	 */
+	rc = keeper->destroying ? -EFAULT : 0;
+lock_out:
+	rcu_read_unlock();
+	return rc;
+}
+
+static long mutex_ioctl_unlock(mutex_ioctl_unlock_arg_t __user *uarg) {
+	mutex_ioctl_unlock_arg_t arg;
+	tgstate_t * state;
+	kernel_mutex_keeper_t * keeper;
+	int rc = 0;
+
+	if (copy_from_user(&arg, uarg, sizeof(arg)))
+		return -EFAULT;
+
+	rcu_read_lock();
+	state = tg_search(RCU);
+	if (state == NULL) {
+		rc = -EFAULT;
+		goto unlock_out;
+	}
+	keeper = mutex_search(state, arg.id, RCU);
+	if (keeper == NULL) {
+		rc = -EFAULT;
+		goto unlock_out;
+	}
+	mutex_wake_single(keeper);
+unlock_out:
+	rcu_read_unlock();
+	return rc;
+}
+
+
 static long mutex_dev_ioctl(struct file *filp, unsigned int cmd,
         unsigned long arg)
 {
-    // TODO
+	switch(cmd) {
+		case MUTEX_IOCTL_CREATE:
+			return mutex_ioctl_create((mutex_ioctl_create_arg_t __user*)arg);
+			break;
+		case MUTEX_IOCTL_DESTROY:
+			return mutex_ioctl_destroy((mutex_ioctl_destroy_arg_t __user*)arg);
+			break;
+		case MUTEX_IOCTL_LOCK:
+			return mutex_ioctl_lock((mutex_ioctl_lock_arg_t __user*)arg);
+			break;
+		case MUTEX_IOCTL_UNLOCK:
+			return mutex_ioctl_unlock((mutex_ioctl_unlock_arg_t __user*)arg);
+			break;
+		default:
+			break;
+	}
     return 0;
 }
 
